3b2837384552f5c48654e1e1bc0651c3d46115df
travisTheRookie <travisb@fireflyint.com>
2021-09-14 21:29:24 -0400

    complete Code class

--- project6/C++ version/Code.cpp
+++ project6/C++ version/Code.cpp
@@ -2,16 +2,16 @@
 
 std::string Code::dest(std::string mnemonic)
 {
-	return "foo";
+    return machineRegister[mnemonic];
 }
 
 std::string Code::comp(std::string mnemonic)
 {
-	return "foo";
+    return operation[mnemonic];
 }
 
 std::string Code::jump(std::string mnemonic)
 {
-	return "foo";
+    return jumpType[mnemonic];
 }
 
