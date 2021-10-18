4dbf0a195a98bb0eb1b16160bac2a563e1700089
travisTheRookie <travisb@fireflyint.com>
2021-10-17 20:10:23 -0400

    complete the main file

--- project6/C++ version/main.cpp
+++ project6/C++ version/main.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
+#include <string>
 
 #include "Code.h"
 #include "Output.h"
 #include "Parser.h"
 #include "SymbolTable.h"
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-	Output output("foo.hack");
-	Parser parser("foo.txt");
-	
-	while (parser.hasMoreCommands())
-	{
-	    parser.advance();
-	    output.writeToBin(parser.command);
-	}
-	output.closeFile();
-	parser.closeFile();
-	
-	SymbolTable symbolTable;
-	
-	std::cout << symbolTable.contains("R16") << "\n";
-	std::cout << symbolTable.getAddress("THIS") << "\n";
-	
-	return 0;
-}
+    if (argc < 2) {
+        std::cout << "ERROR: No source file specified";
+        exit(1);
+    }
 
+    const std::string C_COMMAND_PREFIX = "111";
+    const std::string SOURCE_FILE_NAME = argv[1];
+
+    Parser parser{ SOURCE_FILE_NAME };
+    Output output{ SOURCE_FILE_NAME };
+    SymbolTable symbolTable{};
+    Code code{};
+
+    // first pass
+    while (parser.hasMoreCommands()) {
+        // read next command from source file
+        parser.advance();
+        // label
+        if (parser.commandType() == "L_COMMAND") {
+            if (!symbolTable.contains(parser.symbol())) {
+                symbolTable.addEntry(parser.symbol(), parser.getAddress());
+            }
+        }
+    }
+    // reset file pointer to first line of source file
+    parser.resetFile();
+
+    // second pass
+    while (parser.hasMoreCommands()) {
+        // read next command from source file
+        parser.advance();
+        // addressing command
+        if (parser.commandType() == "A_COMMAND") {
+            if (parser.isConstant()) {
+                output.writeToBin(parser.getConstant());
+            } else {
+                if (!symbolTable.contains(parser.symbol())) {
+                    symbolTable.addEntry(parser.symbol(), "new");
+                }
+                output.writeToBin(symbolTable.getAddress(parser.symbol()));
+            }
+        // computation command
+        } else if (parser.commandType() == "C_COMMAND") { 
+            output.writeToBin(C_COMMAND_PREFIX + code.comp(parser.comp()) + code.dest(parser.dest()) + code.jump(parser.jump()));
+        // label
+        } else if (parser.commandType() != "L_COMMAND") {
+                std::cout << "ERROR: Unexpected command";
+            exit(1);
+        }
+    }
+
+    parser.closeFile();
+    output.closeFile();
+
+    return 0;
+}
