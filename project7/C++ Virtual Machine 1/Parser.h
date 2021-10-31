085b5aac1651362abd4e88ddc5986bce2da28e91
travisTheRookie <travisb@fireflyint.com>
2021-10-31 15:29:52 -0400

    add commandTypes enum and #defines

--- project7/C++ Virtual Machine 1/Parser.h
+++ project7/C++ Virtual Machine 1/Parser.h
@@ -8,6 +8,29 @@ convenient access to their components. In addition, it removes all white
 space and comments.
 */
 
+enum class commandTypes
+{
+    c_arithmetic,
+    c_push,
+    c_pop,
+    c_label,
+    c_goto,
+    c_if,
+    c_function,
+    c_return,
+    c_call,
+};
+
+#define C_ARITHEMTIC commandTypes::c_arithmetic
+#define C_PUSH       commandTypes::c_push
+#define C_POP        commandTypes::c_pop
+#define C_LABEL      commandTypes::c_label
+#define C_GOTO       commandTypes::c_goto
+#define C_IF         commandTypes::c_if
+#define C_FUNCTION   commandTypes::c_function
+#define C_RETURN     commandTypes::c_return
+#define C_CALL       commandTypes::c_call
+
 class Parser
 {
     Parser();
