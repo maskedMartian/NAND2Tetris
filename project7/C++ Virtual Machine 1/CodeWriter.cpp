525ffa8687a1784b7e7287e4cbef8c3d260cf115
travisTheRookie <travisb@fireflyint.com>
2021-11-24 21:26:26 -0500

    add comments to CodeWriter class

--- project7/C++ Virtual Machine 1/CodeWriter.cpp
+++ project7/C++ Virtual Machine 1/CodeWriter.cpp
@@ -1,25 +1,31 @@
 #include "CodeWriter.h"
 
+// Opens the output file/stream and gets ready to write into it
 CodeWriter::CodeWriter(/*output file stream*/)
 {
 
 }
 
+// Informs the code writer that the translation of a new VM file is started
 void CodeWriter::setFileName(std::string fileName)
 {
 
 }
 
+// Writes the assembly code that is that is the translation of the given arithmetic command
 void CodeWriter::writeArithmetic(std::string command)
 {
 
 }
 
+// Writes the assembly code that is the translation of the given command, where command is C_PUSH
+// or C_POP
 void CodeWriter::WritePushPop(PushPopCommands command, std::string segment, int index)
 {
 
 }
 
+// Closes the output file
 void CodeWriter::close()
 {
 
